06f166f49495cbd939057a566f5a2f70c74d73e6
itcast <vigourlee137@gmail.com>
2024-03-17 17:51:12 +0900

    remove

--- tensquareAdmin/config/prod.env.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-module.exports = {
-  NODE_ENV: '"production"',
-  // BASE_API: '"http://192.168.207.131:7300/mock/5c0b42c85b4c7508d4dc568c/1024"'
-  BASE_API: '"http://192.168.110.205:10020"' // 管理员网关
-}
